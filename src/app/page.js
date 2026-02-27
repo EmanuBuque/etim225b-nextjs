324af8a6118b0d07da3dcf46e0f877e9af6130d6
EmanuBuque <manuboter7@gmail.com>
2026-02-27 13:48:05 -0300

    aula css

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,9 @@ function Home() {
   return (
     <div>
       <h1>React com Next JS</h1>
+      <h2>Aula de Introdução</h2>
+      <h3>Leaiute E estilização</h3>
+      <p>Nesta aula iremos entender o us o css global e local.</p>
     </div>
   );
 }
