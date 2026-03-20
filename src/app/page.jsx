075ee701336202b098683270ea9a92f15e8ef52e
EmanuBuque <manuboter7@gmail.com>
2026-03-20 11:44:53 -0300

    exercicio

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,13 @@
 function exercicio () {
     return(
+
         <div className={styles.estilo1}>
             <h1>exemplo 1</h1>
             <p>componente basico para renderizar tela com react</p>
+        </div>
+        <div className={listas.estilo2}>
+            
+
         </div>
     )
 }
